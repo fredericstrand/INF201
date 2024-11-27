7f709f5f952e4a266c604209cf5c409b31e8d8f6
Frederic Strand <strandfrederic@outlook.com>
2024-11-27 22:05:06 +0100

    Still is not working. Seems like there is an issue with pytest

--- exercises/exercise_9/tests/test_add.py
+++ exercises/exercise_9/tests/test_add.py
@@ -1,9 +1,12 @@
 import pytest
-from warm_up import Vector2D
+from ..warm_up import Vector2D
+
+print("Running test_add.py")
 
 def test_add():
     a = Vector2D(1.0, 2.0)
     b = Vector2D(4.0, 4.0)
     c = a + b
-    assert c._x == a._x + b._x
-    assert c._y == a._y + b._y
+    assert c._x == a._x + b._x, "There has been an mistake in adding the x components"
+    assert c._y == a._y + b._y, "There has been an mistake in adding the y components"
+
